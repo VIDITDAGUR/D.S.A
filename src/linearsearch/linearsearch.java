d0b2d514938b1a6a90b6c6c99d177c5066a6636c
Vidit Dagur <viditdagur72@gmail.com>
2026-09-07 16:06:31 +0530

    linear search code - returning element

--- src/linearsearch/linearsearch.java
+++ src/linearsearch/linearsearch.java
@@ -8,7 +8,36 @@ public class linearsearch {
         int target = 4;
         int ans = linearSearch(nums, target);
         System.out.println(ans);
+
+        int ans2 = linearSearch2(nums, target);
+        System.out.println(ans2);
+
     }
+
+    //-----------search the target and return the element--------------------------------
+    static int linearSearch2(int[] arr, int target){
+        if (arr.length == 0){
+            return -1;
+        }
+
+        //run a for loop
+        for (int element : arr) {
+            if (element == target) {
+                return element;
+            }
+        }
+        // this line will execute if none of the return statements above have executed
+        // hence the target not found
+        return -1;
+
+
+    }
+
+
+
+
+
+
     // search in the array: return the index if item found
 //    otherwise if item not found return -1
     static int linearSearch(int[] arr, int target){
