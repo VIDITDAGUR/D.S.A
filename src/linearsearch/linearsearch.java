4fff78a3b27407917ef6c8f37c9b3f830151cea5
Vidit Dagur <viditdagur72@gmail.com>
2026-09-07 15:58:30 +0530

    linear search code

--- src/linearsearch/linearsearch.java
+++ src/linearsearch/linearsearch.java
@@ -1,7 +1,36 @@
 package linearsearch;
 
 public class linearsearch {
+
     static void main(String[] args) {
 
+        int[] nums = {23,43,23,4,2,4,2553,2,3434,2342,24};
+        int target = 4;
+        int ans = linearSearch(nums, target);
+        System.out.println(ans);
     }
+    // search in the array: return the index if item found
+//    otherwise if item not found return -1
+    static int linearSearch(int[] arr, int target){
+        if (arr.length == 0){
+            return -1;
+        }
+
+        //run a for loop
+        for (int index = 0; index < arr.length; index++) {
+            // check for element at every index if it is = target
+            int element = arr[index];
+            if(element == target){
+                return index;
+            }
+        }
+        // this line will execute if none of the return statements above have executed
+        // hence the target not found
+        return -1;
+
+
+    }
+
+
+
 }
